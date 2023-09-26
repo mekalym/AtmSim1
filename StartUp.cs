39d651c9c63e927717245925a32718c17e8a960e
mekalym <omekalimobijiofor@gmail.com>
2023-09-26 09:43:14 +0100

    updates

---
+++ StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMSimulator
+{
+    public class StartUp
+    {
+        public void ConfigureService(IServiceCollection services)
+        {
+
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello");
+                });
+
+            });
+        }
+    }
+}
